a3905cd7ddfec6dabde96800168b3db6e572c2b7
Aakash Varier <aakash021001@gmail.com>
2022-06-21 19:09:02 +0530

    Added chapter and question filter to view_questions

--- client/public/scripts/teacher_page.js
+++ client/public/scripts/teacher_page.js
@@ -27,6 +27,7 @@ function select_syllabus() {
   document.getElementById("filter_button").style.display = "none";
   document.getElementById("syllabus").style.display = "none";
   document.getElementById("semester").style.display = "none";
+  document.getElementById("search_box").style.display = "none";
   document.getElementById("syllabus").style.display = "block";
   for (let i = 0; i < radiobutton.length; i++) {
     if (radiobutton[i].checked) {
@@ -41,11 +42,26 @@ function select_semester() {
   document.getElementById("filter_button").style.display = "none";
   document.getElementById("syllabus").style.display = "none";
   document.getElementById("semester").style.display = "none";
+  document.getElementById("search_box").style.display = "none";
   document.getElementById("semester").style.display = "block";
   for (let i = 0; i < radiobutton.length; i++) {
     if (radiobutton[i].checked) {
       filter = radiobutton[i].value;
-      console.log(filter);
+      // console.log(filter);
+    }
+  }
+}
+
+function display_search_box() {
+  var radiobutton = document.getElementsByName("filter");
+  document.getElementById("filter_button").style.display = "none";
+  document.getElementById("syllabus").style.display = "none";
+  document.getElementById("semester").style.display = "none";
+  document.getElementById("syllabus").style.display = "none";
+  document.getElementById("search_box").style.display = "block";
+  for (let i = 0; i < radiobutton.length; i++) {
+    if (radiobutton[i].checked) {
+      filter = radiobutton[i].value;
     }
   }
 }
@@ -65,6 +81,22 @@ function display_filter_button() {
   console.log(path);
   document.getElementById("filtered_questions").textContent = path;
   document.getElementById("filtered_questions").setAttribute("href", path);
+}
+
+function search_item() {
+  filter_val = document.getElementById("filter_input").value;
+  console.log(filter);
+  console.log(filter_val);
+
+  if (filter_val == undefined) {
+    document.write("Enter a valid search");
+  } else {
+    const path = "/teacher_page/view-questions/" + filter + "/" + filter_val;
+    document.getElementById("filter_button").style.display = "block";
+    console.log;
+    document.getElementById("filtered_questions").textContent = path;
+    document.getElementById("filtered_questions").setAttribute("href", path);
+  }
 }
 
 let generator_semester_value, generator_syllabus_value;
--- server/src/routes_function/view_questions.js
+++ server/src/routes_function/view_questions.js
@@ -4,8 +4,6 @@ exports.view_all_questions = async (req, res) => {
   try {
     const { filter } = req.params;
     const { filter_value } = req.params;
-    // filter = filter.toLowerCase();
-    // console.log(filter);
     if (filter == undefined) {
       await pool.query(
         "SELECT * FROM question_paper",
@@ -18,8 +16,6 @@ exports.view_all_questions = async (req, res) => {
           });
         }
       );
-      // console.log(req.params);
-      // return res.json(view_questions.rows);
     } else {
       if (filter.toLowerCase() === "syllabus") {
         await pool.query(
@@ -34,8 +30,6 @@ exports.view_all_questions = async (req, res) => {
             });
           }
         );
-        // console.log(req.params);
-        // return res.json(view_questions.rows);
       } else if (filter.toLowerCase() === "semester") {
         await pool.query(
           "SELECT * FROM question_paper WHERE semester = $1",
@@ -49,43 +43,32 @@ exports.view_all_questions = async (req, res) => {
             });
           }
         );
-        // console.log(req.params);
-        // return res.json(view_questions.rows);
       } else if (filter.toLowerCase() === "chapter") {
-        const view_questions = await pool.query(
-          "SELECT * FROM question_paper WHERE LOWER(chapter) = $1",
-          [filter_value.toLowerCase()]
+        await pool.query(
+          "SELECT * FROM question_paper WHERE LOWER(chapter) LIKE $1",
+          [filter_value.toLowerCase() + "%"],
+          function (err, data, fields) {
+            if (err) throw err;
+            return res.render("teacher_page.ejs", {
+              path: req.url,
+              userdata: Object.values(data.rows),
+              user: req.user.name,
+            });
+          }
         );
-        // console.log(req.params);
-        return res.json(view_questions.rows);
-      } else if (filter.toLowerCase() === "unit") {
-        const view_questions = await pool.query(
-          "SELECT * FROM question_paper WHERE unit = $1",
-          [filter_value]
-        );
-        // console.log(req.params);
-        return res.json(view_questions.rows);
-      } else if (filter.toLowerCase() === "marks") {
-        const view_questions = await pool.query(
-          "SELECT * FROM question_paper WHERE marks = $1",
-          [filter_value]
-        );
-        // console.log(req.params);
-        return res.json(view_questions.rows);
-      } else if (filter.toLowerCase() === "priority") {
-        const view_questions = await pool.query(
-          "SELECT * FROM question_paper WHERE priority = $1",
-          [filter_value]
-        );
-        // console.log(req.params);
-        return res.json(view_questions.rows);
       } else if (filter.toLowerCase() === "questions") {
-        const view_questions = await pool.query(
-          "SELECT * FROM question_paper WHERE LOWER(question) = $1",
-          [filter_value.toLowerCase()]
+        await pool.query(
+          "SELECT * FROM question_paper WHERE LOWER(question) LIKE $1",
+          [filter_value.toLowerCase() + "%"],
+          function (err, data, fields) {
+            if (err) throw err;
+            return res.render("teacher_page.ejs", {
+              path: req.url,
+              userdata: Object.values(data.rows),
+              user: req.user.name,
+            });
+          }
         );
-        // console.log(req.params);
-        return res.json(view_questions.rows);
       } else {
         const view_questions = await pool.query("SELECT * FROM question_paper");
         return res.json(view_questions.rows);
