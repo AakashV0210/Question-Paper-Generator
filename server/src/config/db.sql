5c0770a1fc9fb28ee2ab3fa7b9cfbc60322f8722
Aakash Varier <aakash021001@gmail.com>
2022-06-22 23:04:01 +0530

    added functionality where duplicate entried do not get added to the database anymore

--- server/src/config/db.sql
+++ server/src/config/db.sql
@@ -8,15 +8,28 @@ UNIQUE (email));
 -- VALUES ('Conor', 'conbailey@gmail.com', 'password');
 
 CREATE TABLE question_paper(
-    id SERIAL PRIMARY KEY,
-    syllabus VARCHAR(10),
-    semester INT,
-    chapter VARCHAR(25),
-    unit INT,
-    marks INT,
-    priority INT,
-    question VARCHAR(255)
+    id SERIAL PRIMARY KEY NOT NULL,
+    syllabus VARCHAR(10) NOT NULL,
+    semester INT NOT NULL,
+    chapter VARCHAR(25) NOT NULL,
+    unit INT NOT NULL,
+    marks INT NOT NULL,
+    priority INT NOT NULL,
+    question VARCHAR(255) UNIQUE
 );
+
+-- CREATE TABLE question_paper(
+--     id SERIAL PRIMARY KEY,
+--     syllabus VARCHAR(10),
+--     semester INT,
+--     chapter VARCHAR(25),
+--     unit INT,
+--     marks INT,
+--     priority INT,
+--     question VARCHAR(255)
+-- );
+
+-- ALTER TABLE question_paper ADD CONSTRAINT unique_question UNIQUE (question);
 
 -- CREATE TABLE todo(
 --     todo_id SERIAL PRIMARY KEY,
