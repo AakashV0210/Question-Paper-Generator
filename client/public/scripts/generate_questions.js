8499699aaa22a716f05978b4193a9fee2318dffd
Aakash Varier <aakash021001@gmail.com>
2022-06-24 01:03:35 +0530

    fixed radio buttons in generate questions page

--- client/public/scripts/generate_questions.js
+++ client/public/scripts/generate_questions.js
@@ -1,6 +1,17 @@
 let generator_semester_value, generator_syllabus_value;
 
-function display_generate_semester() {
+function display_generate_semester_form() {
+  document.getElementById("question_generator_semester").style.display = "none";
+
+  var semester_radio_button = document.getElementsByName("generator_semester");
+
+  //uncheck semester radiobuttons on load
+  for (let i = 0; i < semester_radio_button.length; i++) {
+    if (semester_radio_button[i].checked) {
+      semester_radio_button[i].checked = false;
+    }
+  }
+
   var syllabus_radio_button = document.getElementsByName("generator_syllabus");
   for (let i = 0; i < syllabus_radio_button.length; i++) {
     if (syllabus_radio_button[i].checked) {
@@ -11,23 +22,32 @@ function display_generate_semester() {
   }
 }
 
-function display_generate_button() {
+function validate_question_generate() {
+  let count = 0;
+
   var semester_radio_button = document.getElementsByName("generator_semester");
+
   for (let i = 0; i < semester_radio_button.length; i++) {
     if (semester_radio_button[i].checked) {
       generator_semester_value = semester_radio_button[i].value;
-      document.getElementById("generate_button").style.display = "block";
-      break;
+      count++;
     }
   }
-  console.log(generator_syllabus_value);
-  console.log(generator_semester_value);
 
+  if (count == 1) {
+    display_generated_questions();
+  } else {
+    alert("You must select something!");
+    return false;
+  }
+}
+
+function display_generated_questions() {
   const path =
     "/teacher_page/generate-questions/" +
     generator_syllabus_value +
     "/" +
     generator_semester_value;
-  document.getElementById("generate_question_paper").textContent = path;
-  document.getElementById("generate_question_paper").setAttribute("href", path);
+
+  document.getElementById("generator_form").setAttribute("action", path);
 }
