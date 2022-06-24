15357603567d80049992c89e76661ef5f50ee066
Namith Np <namithnp2001@gmail.com>
2022-06-25 02:14:07 +0530

    Major css changes

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./client/views/*.ejs", "./client/partials/*.ejs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
